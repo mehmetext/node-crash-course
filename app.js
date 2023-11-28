ab2c3fab0dbd36e6b6e54784a61d073703ed3aab
Mehmet <mehmetext68@gmail.com>
2023-11-28 13:52:04 +0300

    add writeHead function

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var http = require("http");
 
 http
   .createServer((req, res) => {
-    res.write("Hello, world!");
+    res.writeHead(200, null, {
+      "Content-Type": "text/html",
+    });
+    res.write("<h1>Hello, world!</h1>");
     res.end("end");
   })
   .listen(3000);
