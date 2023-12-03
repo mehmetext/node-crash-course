9d3d2230e40af2e7bc02754fba8af00b4ad2856e
Mehmet <mehmetext68@gmail.com>
2023-12-03 20:18:54 +0300

    add middleware to categoriesRoute

--- src/routes/categoriesRoute.js
+++ src/routes/categoriesRoute.js
@@ -2,11 +2,22 @@ const express = require("express");
 
 const categoriesRoute = express.Router();
 
+categoriesRoute.use((req, res, next) => {
+  req.foo = "bar";
+  console.log("CategoriesRoute Middleware");
+
+  next();
+});
+
 categoriesRoute.get("/", (req, res) => {
+  console.log("req.foo:", req.foo);
+
   res.send("Categories");
 });
 
 categoriesRoute.get("/electronics", (req, res) => {
+  console.log("req.foo:", req.foo);
+
   res.send("Electronics");
 });
 
