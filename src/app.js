5218d8245b7b1771d61a8e33a04414a8f8c19872
Mehmet <mehmetext68@gmail.com>
2023-12-03 12:24:20 +0300

    add static public folder

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use("/public", express.static("public"));
+
 app.get("/", (req, res) => {
   res.send("Hello, world! (from /)");
 });
