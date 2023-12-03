cfbbf5e0566ad7e613de4a492cb539a280af9960
Mehmet <mehmetext68@gmail.com>
2023-12-03 13:17:40 +0300

    add simple middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.use("/public", express.static("public"));
 
+app.use((req, res, next) => {
+  console.log("url:", req.url);
+  console.log("time:", new Date());
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, world! (from /)");
 });
