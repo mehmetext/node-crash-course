f86c51f3cfef8ad713ccc92e3de624c5b8de974e
Mehmet <mehmetext68@gmail.com>
2023-12-03 11:31:17 +0300

    appendFile

--- fileSystem.js
+++ fileSystem.js
@@ -1,11 +1,6 @@
-var http = require("http");
 var fs = require("fs");
 
-http
-  .createServer((req, res) => {
-    fs.readFile("page.html", (err, data) => {
-      // res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end(data);
-    });
-  })
-  .listen(3000);
+fs.appendFile("text.txt", "\nSecond", (err) => {
+  if (err) throw err;
+  console.log("Succesfull");
+});
