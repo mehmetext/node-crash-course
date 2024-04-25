5e0f243727653bda2b918e119228d630943b4f6f
Mehmet <mehmetext68@gmail.com>
2024-04-25 14:16:18 +0300

    using read and write functions in fs

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const { add, sub } = require("./math");
+const fs = require("fs");
 
-console.log(add(2, 3));
-console.log(sub(2, 3));
+//sync (blocking)
+fs.writeFileSync("log.txt", "text");
+
+//async (non-blocking)
+fs.writeFile("log.txt", "text2", (err) => {
+  if (err) throw err;
+});
+
+const readF = fs.readFileSync("./log-2.txt", "utf-8");
+console.log("readFileSync", readF);
+
+fs.readFile("log-2.txt", "utf-8", (err, data) => {
+  if (err) throw err;
+
+  console.log("readFile", data);
+});
